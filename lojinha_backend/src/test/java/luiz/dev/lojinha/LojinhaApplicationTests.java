e64a7cd4b594572e9598e455865dce4ad1fdc55d
Luiz Felipe <alucardlf1911@gmail.com>
2026-01-20 15:37:24 -0300

    Criando o projeto

---
+++ lojinha_backend/src/test/java/luiz/dev/lojinha/LojinhaApplicationTests.java
@@ -0,0 +1,13 @@
+package luiz.dev.lojinha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojinhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
